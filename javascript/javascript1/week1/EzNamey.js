2f0b9ef8e1ad70a60909310385af3f0e4e71b9bb
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-06 09:37:53 +0200

    Added startup name calculator

---
+++ javascript/javascript1/week1/EzNamey.js
@@ -0,0 +1,5 @@
+let firstWords=["Easy","Awesome","Corporate","Nest","Mind","Brain","Sun","Mist","Quest","Gravity"];
+let secondWords=["Tech","Innovation","Storm","Solutions","Tree","Plum","Mobility","Windy","Smart","Data"];
+let startupName=firstWords[4]+secondWords[4];
+const len=startupName.length;
+console.log("The startup: "+startupName+" "+"contains"+" "+len+" "+"characters");

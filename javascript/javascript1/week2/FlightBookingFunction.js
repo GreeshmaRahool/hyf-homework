cbdc5ae01cd634b42e5d926580560695c2568af8
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-12 14:02:20 +0200

    Committed Flight booking function

---
+++ javascript/javascript1/week2/FlightBookingFunction.js
@@ -0,0 +1,21 @@
+function getFullname (firstName,surName,useFormalName,gender)
+{
+    if(useFormalName==true&&gender==='male')
+    {
+        return `Lord ${firstName}${surName}`;
+    }
+   
+    else if(useFormalName==true&&gender==='female')
+    {
+        return `Lady ${firstName}${surName}`;
+    }
+    else if(useFormalName==false&&(gender==='male'||gender==='female'))
+    {
+        return `${firstName}${surName}`;
+    }   
+   
+}
+const fullName=getFullname("Benjamin", " Hughes",true,'male');
+const fullName1=getFullname("Maria", " Joseph",false,'female');
+const fullName2=getFullname("Freja", " Nielson",true,'female');
+console.log(`Here is the travellers list: \n${fullName}\n${fullName1}\n${fullName2}`);

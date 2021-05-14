34e41571483f13a5235fbf91f077998f027268d0
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-14 03:57:30 +0200

    Added Event application

--- javascript/javascript1/week2/EventApplication.js
+++ javascript/javascript1/week2/EventApplication.js
@@ -3,7 +3,6 @@ function getEventWeekday(noOfFutureEventDays)
 {
     const curWeekDay= new Date();
     const futureEventDay=(noOfFutureEventDays + curWeekDay.getDay())% 7;
-    console.log(futureEventDay);
     const arrWeekDays=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     console.log(`The event will be held on ${arrWeekDays[futureEventDay]}`);
 }

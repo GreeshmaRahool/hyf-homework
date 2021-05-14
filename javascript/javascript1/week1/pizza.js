e7fd714a573b63e58dd177cd98e93dd543fb7078
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-14 19:49:32 +0200

    Modified with review comments

--- javascript/javascript1/week1/pizza.js
+++ javascript/javascript1/week1/pizza.js
@@ -1,21 +1,19 @@
 console.log("I love pizza");
-const favPizza="Mozarella"
-let pizzaPrice=110;
-const quantity=3;
-let strFamily;
-let totalPrice
-let familyPizza=true;
-if(familyPizza==true)
+const favPizza = "Mozarella"
+const pizzaPrice = 110;
+const quantity =3;
+let strFamily,totalPrice;
+let familyPizza = true;
+if(familyPizza == true)
 {
-    totalPrice=(pizzaPrice*quantity)*2;
-    strFamily="and family pizza";
+    totalPrice = (pizzaPrice*quantity)*2;
+    strFamily = "and family pizza";
     console.log("New pizza order: "+quantity+" "+strFamily+" "+favPizza+"."+"Total cost for the order is: "+totalPrice);
 }
-familyPizza=false;
-if(familyPizza==false)
+familyPizza = false;
+if(familyPizza == false)
 {
-    totalPrice=pizzaPrice*quantity;
-    strFamily="and not a family pizza";
+    totalPrice = pizzaPrice*quantity;
+    strFamily = "and not a family pizza";
     console.log("New pizza order: "+quantity+" "+strFamily+" "+favPizza+"."+"Total cost for the order is:"+totalPrice);
 }
-//console.log("New pizza order: "+favPizza+"."+" The price of the pizza is: "+pizzaPrice);

750b93f6f55fd28a640afdb624e03af403e6556c
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-20 11:07:24 +0200

    Updated with empty string , null and white space check

--- javascript/javascript1/week2/FlightBookingFunction.js
+++ javascript/javascript1/week2/FlightBookingFunction.js
@@ -1,6 +1,6 @@
 function getFullname (firstName,surName,useFormalName,gender)
 {
-    if(firstName === " " || surName=== " ")
+    if(!firstName || !surName || firstName === " " || surName === " ")
     {
         console.log('Provide valid name!');
     }
@@ -8,22 +8,27 @@ function getFullname (firstName,surName,useFormalName,gender)
     {
         console.log('Provide valid name!');
     }
-    else if(useFormalName==true&&gender==='male')
+    else if(useFormalName == true && gender === 'male')
     {
         return `Lord ${firstName}${surName}`;
     }
    
-    else if(useFormalName==true&&gender==='female')
+    else if(useFormalName == true && gender === 'female')
     {
         return `Lady ${firstName}${surName}`;
     }
-    else if(useFormalName==false&&(gender==='male'||gender==='female'))
+    else if(useFormalName == false && (gender==='male' || gender === 'female'))
     {
         return `${firstName}${surName}`;
     }   
    
 }
-const fullName=getFullname("Benjamin", " Hughes",true,'male');
-const fullName1=getFullname("Maria", " Joseph",false,'female');
-const fullName2=getFullname("Freja", " Nielson",true,'female');
+const fullName = getFullname("Benjamin", " Hughes",true,'male');
+const fullName1 = getFullname("Maria", " Joseph",false,'female');
+const fullName2 = getFullname("Freja", " Nielson",true,'female');
+
+getFullname(" "," ",false,"male");
+getFullname("","",false,"male");
+getFullname(false,"male");
+
 console.log(`Here is the travellers list: \n${fullName}\n${fullName1}\n${fullName2}`);

d9be08f68b8c63b89281ecb62b993c5ea55fb884
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-20 11:13:48 +0200

    Simplified switch-case block by comparing one value

--- javascript/javascript1/week2/WeatherWear.js
+++ javascript/javascript1/week2/WeatherWear.js
@@ -7,10 +7,10 @@ function getClothsToWear(temperature)
     case curTemperature <= -10:
         console.log(`Wear cold-weather attire with a heavy coat, gloves, scarf, and hat.Wool or cotton would be good choice!`);
         break;
-    case curTemperature > -10 && curTemperature <= 7:
+    case  curTemperature <= 7:
         console.log(`Wear cold-weather attire with a heavy coat, gloves, scarf, and hat.`);
         break;
-    case curTemperature > 7 && curTemperature <= 16:
+    case curTemperature <= 16:
         console.log(`Wear thicker sweaters, jumpers, and turtlenecks`);
         break;s
     case curTemperature > 16 :
@@ -21,4 +21,7 @@ function getClothsToWear(temperature)
     }
 }
 getClothsToWear(-14);
+getClothsToWear(5);
+getClothsToWear(14);
+getClothsToWear(20);
     

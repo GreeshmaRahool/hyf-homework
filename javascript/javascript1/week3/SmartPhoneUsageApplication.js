8167c978d9d6096ba395816b9edff9436ff2fed6
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-05-28 23:01:15 +0200

    Implimented review comments

--- javascript/javascript1/week3/SmartPhoneUsageApplication.js
+++ javascript/javascript1/week3/SmartPhoneUsageApplication.js
@@ -23,14 +23,13 @@ addActivity("21/7-18", "TikTok", 38);
 function showStatus(activities)
 { 
     let totalDuration = 0;
-    if(activities.length !== 0)
-    {      
+         
         for(i = 0 ; i < activities.length ; i++ )
         {         
             totalDuration += activities[i].duration;
         }
         return totalDuration;
-     }
+  
 }
 const durationSum = showStatus(activities);
 console.log(`You have added ${activities.length} activities. They amount to ${durationSum} min. of usage`);
@@ -38,14 +37,15 @@ console.log(`You have added ${activities.length} activities. They amount to ${du
 //Usage limit
 
 function limitUsage()
-{   const usageLimit = 100;
+{   
+    const usageLimit = 100;
     if(durationSum > usageLimit)
     {
         console.log(`You have reached your limit, no more smartphoning for you!`)
     }
 //Extra feature
 //Setting reminder for 80% of set limit
-    percntageOfUsage = (durationSum * 100) / usageLimit
+    const percntageOfUsage = (durationSum * 100) / usageLimit
     if(percntageOfUsage === 80)
     {
         console.log(`Warning : Your screen time is 80% of set limit! `)

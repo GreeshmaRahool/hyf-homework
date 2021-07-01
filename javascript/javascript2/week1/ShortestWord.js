dd8a2d3156dbea848c15c9f6891a1d558de34153
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-07-01 14:50:12 +0200

    Implimented review changes

--- javascript/javascript2/week1/ShortestWord.js
+++ javascript/javascript2/week1/ShortestWord.js
@@ -1,19 +1,26 @@
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
-let shortestWord = "";
+// let shortestWord = "";
 
-function getShortestWord(danishWords)
-{
-    let wordToCompare = danishWords[0].length;
-    for(let i = 0; i < danishWords.length; i ++)
-    {
-        if(wordToCompare >= danishWords[i].length)
-        {
-            wordToCompare = danishWords[i].length;
-            shortestWord =   danishWords[i];
-        }
-    }
-    return shortestWord;
-        
-}
-const result = getShortestWord(danishWords);
-console.log(result);
+// function getShortestWord(danishWords)
+// {
+//     let wordToCompare = danishWords[0].length;
+//     for(let i = 0; i < danishWords.length; i ++)
+//     {
+//         if(wordToCompare >= danishWords[i].length)
+//         {
+//             wordToCompare = danishWords[i].length;
+//             shortestWord =   danishWords[i];
+//         }
+//     }
+//     return shortestWord;
+     
+// }
+// const result = getShortestWord(danishWords);
+// console.log(result);
+
+let shortestWord = danishWords[0].length; 
+danishWords.forEach(word =>{
+  if(shortestWord > word.length) 
+  shortestWord = word;
+});
+console.log(shortestWord)

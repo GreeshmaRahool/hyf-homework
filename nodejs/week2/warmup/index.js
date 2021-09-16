e809ca88009e70c673a4ee0aa9ee2886b41a9784
GreeshmaRahool <greeshma.rahool@outlook.com>
2021-09-16 11:18:47 +0200

    Update warm up

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -6,3 +6,4 @@ const numbersRouter = require('./numbersRouter');
 app.get("/", (request, response) => response.send("nodejs week2 homework"));
 
 app.use('/numbers', numbersRouter);
+app.listen(3000, () => console.log('Calculator:listening on port 3000'));
